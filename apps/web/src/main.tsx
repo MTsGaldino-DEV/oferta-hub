c7f945d1f8b0fd28967a33ef57f5a07215775ac5
Matheus <devmatheus94@gmail.com>
2026-08-23 15:56:57 -0300

    build(web): setup Tailwind v4 + alias @/* pra migracao da sidebar

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.js';
 import './styles.css';
+import './tailwind.css';
 import { applyStoredTheme } from './theme.js';
 
 applyStoredTheme();
